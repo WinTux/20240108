79643f91b4394b24d6d02f3247b2bfa8c168516a
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2024-01-08 21:13:14 -0400

    Agregar archivos de proyecto.

---
+++ ProyectoEjemplo/Controllers/Ejemplo1Controller.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoEjemplo.Controllers
+{
+    [Route("api/ejemplo1")]
+    public class Ejemplo1Controller : Controller
+    {
+        [HttpGet("prueba1")]
+        public async Task<IActionResult> prueba1()
+        {
+            try
+            {
+                return Ok("Esta es una prueba");
+            }
+            catch { 
+                return BadRequest();
+            }
+        }
+        [Produces("text/html")]
+        [HttpGet("prueba2")]
+        public async Task<IActionResult> prueba2()
+        {
+            try
+            {
+                return Ok("<h2><b><i>Esta es otra prueba</i></b></h2>");
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("prueba3/{val}")]
+        public async Task<IActionResult> prueba3(string val)
+        {
+            try
+            {
+                return Ok("Buenas noches Sr. " + val);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ ProyectoEjemplo/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace ProyectoEjemplo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers(
+                opciones => opciones.OutputFormatters.OfType<StringOutputFormatter>().Single().SupportedMediaTypes.Add("text/html")
+            );
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
